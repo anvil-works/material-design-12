4d36a7eb6ddd4ceb3590c2b7b9b1ed4dd02dd58b
pete@anvil.works <pete@anvil.works>
2025-05-17 19:47:06 +0000

    Created ServerModule1 and LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +11,9 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def primary_color_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name = self.text_box_1.text
+    self.headline_1.text = f"Hello, {name}"
+    anvil.server.call('say_hello', name)
